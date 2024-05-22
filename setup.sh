4e0bac008753b89ec326714ec9fb5e02e821fb01
VnzVPN <69085043+xhidrolix@users.noreply.github.com>
2024-05-22 23:40:48 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Langkah 1: Update dan upgrade sistem
+echo "ᴛʜᴀɴᴋꜱ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ꜱᴄʀɪᴘᴛ ʙʏ ᴛ.ᴍᴇ/ᴠɴᴢᴠᴘɴ"
+sleep 3
+echo "ᴘʀᴏꜱᴇꜱ ᴍᴇɴᴏɴᴀᴋᴛɪꜰᴋᴀɴ ɪᴘᴠ6"
+sleep 2
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sleep 3
+echo "ᴘʀᴏᴄᴇꜱꜱ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ꜰɪʟᴇ ʙᴀꜱɪᴄ"
+sleep 2
+apt update -y && apt upgrade -y
+sleep 2
+echo "ᴜᴘᴅᴀᴛᴇ & ᴜᴘɢʀᴀᴅᴇ ᴘᴀᴄᴋᴀɢᴇ ᴄᴏᴍᴘʟᴇᴛᴇ"
+sleep 2
+echo "ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ..."
+sleep 3
+
+# Langkah 2: Mengunduh dan menjalankan skrip instalasi
+echo "ʟᴇᴛꜱ'ɢᴏ ᴛᴏ ɪɴꜱᴛᴀʟʟɪɴɢ"
+sleep 4
+wget https://raw.githubusercontent.com/xhidrolix/vnzscnew/main/install.sh && chmod +x install.sh && ./install.sh
