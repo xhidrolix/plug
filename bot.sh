3e2b808de3d33011aaf1bdb160eee9cafaa3ed11
VnzVPN <69085043+xhidrolix@users.noreply.github.com>
2024-05-27 22:52:57 +0700

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,10 @@
+# // Menginstall Bot Notifikasi
+apt install python3-pip -y
+pip3 install telegram-send
+echo "LABEL=/boot /boot ext2 default, ro 1 2" >> /etc/
+clear
+echo ""
+echo "Kunjungi T.me/VnzVPNNOTIF_BOT "
+echo "masukan kode di bawah ke bot tele di atas"
+printf "6786147163:AAGNNXdGlN4PZGSdEH0ceRhxTzIZlmIjCf4" | telegram-send --configure
+clear
