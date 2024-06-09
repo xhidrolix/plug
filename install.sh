dd768802b6e992c3534bc19d03ccbcfc63a58c78
VnzVPN <69085043+xhidrolix@users.noreply.github.com>
2024-06-09 18:01:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -276,6 +276,25 @@ wget -O /etc/noobzvpns/key.pem "https://github.com/noobz-id/noobzvpns/raw/master
 chmod +x /etc/noobzvpns/*
 clear
 
+# Menginstall VNSTAT
+apt -y install vnstat > /dev/null 2>&1
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev > /dev/null 2>&1
+wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+/etc/init.d/vnstat status
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
+clear
+
 # Menginstall Plugin
 wget https://github.com/praiman99/Plugin-FN/raw/Beginner/plugin.sh ; chmod 777 plugin.sh ; ./plugin.sh ; rm -fr plugin.sh
 
